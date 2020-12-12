8287582cc50b0bc9052ce468c405f3da8f92c1dc
Venkata Krishna Jonnabhatla <vkjonnabhatla@gmail.com>
2020-12-12 13:07:24 -0500

    spring consul example project

---
+++ src/main/java/com/consul/config/SpringConsulConfigApplication.java
@@ -0,0 +1,13 @@
+package com.consul.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConsulConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConsulConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consul/config/SpringConsulConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consul.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConsulConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
