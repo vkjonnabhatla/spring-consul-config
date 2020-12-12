b2a04b0ddf865add8fbdc97c3c544d45c92b7394
Venkata Krishna Jonnabhatla <vkjonnabhatla@gmail.com>
2020-12-12 16:46:09 -0500

    first commit

--- src/main/java/com/consul/config/SpringConsulConfigApplication.java
+++ src/main/java/com/consul/config/SpringConsulConfigApplication.java
@@ -1,13 +1,35 @@
 package com.consul.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableConfigurationProperties(value = AppConfig.class)
 public class SpringConsulConfigApplication {
 
+//	@Value("${spring.app.username}")
+//	private String userName;
+//	
+//	@Value("${spring.app.password}")
+//	private String password;
+	
+	@Autowired
+	private AppConfig appConfig;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringConsulConfigApplication.class, args);
+	}
+	
+	@GetMapping("/user")
+	public String getValue() {
+		return "Welcome " + appConfig.getPassword();
 	}
 
 }
